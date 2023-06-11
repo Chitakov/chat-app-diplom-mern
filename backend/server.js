209da200bfc2c9f2fc0040ea61d12bee58d015c8
Chitakov <chitakov2606nikita@gmail.com>
2023-06-11 20:51:42 +0300

    Added .env work

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+const colors = require("colors");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
-const app = express();
 dotenv.config();
+connectDB();
+const app = express();
+
+console.log(process.env.REACT_APP_PORT);
+console.log(process.env.REACT_APP_MONGO_URI);
+console.log(process.env.REACT_APP_JWT_SECRET);
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
+app.use("/api/user", userRoutes);
 
-app.get("/api/chat/:id", (req, res) => {
-  //   console.log(req.params.id);
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+const PORT = process.env.REACT_APP_PORT || 5000;
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(5000, console.log(`Server started on port ${PORT}`));
+app.listen(5000, console.log(`Server started on port ${PORT}`.yellow.bold));
