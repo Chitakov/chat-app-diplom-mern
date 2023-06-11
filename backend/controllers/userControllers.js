2e7881c73eff2ef34cc2a31b8ca6556f40ad2d70
Chitakov <chitakov2606nikita@gmail.com>
2023-06-11 21:38:01 +0300

    Added password encrypt

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -32,7 +32,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
-      isAdmin: user.isAdmin,
+      // isAdmin: user.isAdmin,
       pic: user.pic,
       token: generateToken(user._id),
     });
@@ -46,21 +46,21 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route           POST /api/users/login
 //@access          Public
 const authUser = asyncHandler(async (req, res) => {
-  //   const { email, password } = req.body;
-  //   const user = await User.findOne({ email });
-  //   if (user && (await user.matchPassword(password))) {
-  //     res.json({
-  //       _id: user._id,
-  //       name: user.name,
-  //       email: user.email,
-  //       isAdmin: user.isAdmin,
-  //       pic: user.pic,
-  //       token: generateToken(user._id),
-  //     });
-  //   } else {
-  //     res.status(401);
-  //     throw new Error("Invalid Email or Password");
-  //   }
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      // isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
 });
 
 module.exports = { registerUser, authUser };
